efc36cb2365ecb87d53e9f677978a8a878d37797
Sam Williams <swilliams96@outlook.com>
2024-04-21 16:44:21 +0100

    Implement basic swipe controls for loading the next video

--- ui/src/components/Player/Player.tsx
+++ ui/src/components/Player/Player.tsx
@@ -1,13 +1,46 @@
 import "./Player.css";
-import { useEffect, useState } from "react";
+import { TouchEventHandler, useEffect, useState } from "react";
 import { PlayerOverlay } from "../PlayerOverlay/PlayerOverlay";
 import { PlayerVideo } from "../PlayerVideo/PlayerVideo";
 import Video from "../../models/Video";
 import { fetchVideo } from "../../services/VideoService";
 
+const minSwipeDistance = 60;
+
 export const Player = () => {
   const [videos, setVideos] = useState<Video[]>([]);
   const [activeVideoIndex, setActiveVideoIndex] = useState<number>(0);
+  const [touchStartY, setTouchStartY] = useState(null);
+  const [touchEndY, setTouchEndY] = useState(null);
+
+  const onTouchStart = (e: any) => {
+    setTouchEndY(null);
+    setTouchStartY(e.targetTouches[0].clientY);
+  };
+
+  const onTouchEnd = () => {
+    if (!touchStartY || !touchEndY) return;
+
+    const distance = touchStartY - touchEndY;
+
+    if (distance > minSwipeDistance) {
+      if (activeVideoIndex === videos.length - 2) {
+        // Always have the next two videos lined up to play
+        (async () => {
+          let newVideo = await fetchVideo();
+          setVideos([ ...videos, newVideo ]);
+        })();
+      }
+
+      setActiveVideoIndex(activeVideoIndex + 1);
+    }
+    
+    if (distance < -minSwipeDistance && activeVideoIndex > 0) {
+      setActiveVideoIndex(activeVideoIndex - 1);
+    }
+  };
+
+  const onTouchMove = (e: any) => setTouchEndY(e.targetTouches[0].clientY);
 
   useEffect(() => {
     (async () => {
@@ -23,7 +56,7 @@ export const Player = () => {
   let activeVideo = videos[activeVideoIndex];
 
   return (
-    <div className="player">
+    <div className="player" onTouchStart={onTouchStart} onTouchEnd={onTouchEnd} onTouchMove={onTouchMove}>
       <PlayerVideo video={activeVideo} />
       <PlayerOverlay activeVideo={activeVideo} />
     </div>
