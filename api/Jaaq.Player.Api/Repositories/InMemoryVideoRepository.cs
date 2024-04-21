3bfa9de1be7fc4f0ced6406c87cf3a1aefd246dc
Sam Williams <swilliams96@outlook.com>
2024-04-21 17:37:33 +0100

    Ensure videos aren't returned back to back for better UX

--- api/Jaaq.Player.Api/Repositories/InMemoryVideoRepository.cs
+++ api/Jaaq.Player.Api/Repositories/InMemoryVideoRepository.cs
@@ -5,7 +5,7 @@ namespace Jaaq.Player.Api.Repositories
 {
     public class InMemoryVideoRepository : IVideoRepository
     {
-        private List<Video> _videos = new()
+        private readonly List<Video> _videos = new List<Video>()
         {
             new(Guid.NewGuid(), "Desert Stroll", "See something new!", "https://videos.pexels.com/video-files/20770858/20770858-hd_1080_1920_30fps.mp4", InMemoryUserRepository.VideoPoster.Username, DateTime.UtcNow.AddDays(-1)),
             new(Guid.NewGuid(), "Tasty Food", "Cook some food you really enjoy, as long as it's healthy 😋", "https://videos.pexels.com/video-files/10200314/10200314-hd_2160_3840_25fps.mp4", InMemoryUserRepository.VideoPoster.Username, DateTime.UtcNow.AddDays(-2)),
@@ -13,11 +13,22 @@ namespace Jaaq.Player.Api.Repositories
             new(Guid.NewGuid(), "Get out in Nature", "Spend some time out in nature! (but watch out for bears... 😱)", "https://videos.pexels.com/video-files/18148735/18148735-sd_540_960_30fps.mp4", InMemoryUserRepository.VideoPoster.Username, DateTime.UtcNow.AddDays(-4)),
             new(Guid.NewGuid(), "Exercise", "Exercise is always good for your mental health, especially if it's with friends or loved ones!", "https://videos.pexels.com/video-files/20196832/20196832-sd_540_960_30fps.mp4", InMemoryUserRepository.VideoPoster.Username, DateTime.UtcNow.AddDays(-5)),
             new(Guid.NewGuid(), "Hot Drinks", "Have a hot drink and relax, though be careful not to have too much caffeine 😖", "https://videos.pexels.com/video-files/20125021/20125021-sd_540_960_30fps.mp4", InMemoryUserRepository.VideoPoster.Username, DateTime.UtcNow.AddDays(-6)),
-        };
+        }
+        .OrderBy(x => Guid.NewGuid())
+        .ToList();
+
+        private int _lastReturnedVideoIndex = 0;
 
         public Task<Video> GetRandomVideoAsync()
         {
-            var video = _videos.OrderBy(x => Guid.NewGuid()).First();
+            if (_lastReturnedVideoIndex == _videos.Count)
+            {
+                _lastReturnedVideoIndex = 0;
+            }
+
+            var video = _videos.Skip(_lastReturnedVideoIndex).First();
+            _lastReturnedVideoIndex++;
+
             return Task.FromResult(video);
         }
     }
